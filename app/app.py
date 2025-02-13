2cd5bd4b31fe4ef09f443d6afd60f60fdd70621e
Bao Bui-Quang <quangbao.dev@gmail.com>
2025-02-13 21:22:33 +0700

    update

--- app/app.py
+++ app/app.py
@@ -1,31 +1,30 @@
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware as FAPI_CORSMiddleware
+from fastapi.staticfiles import StaticFiles as FAPI_StaticFiles
+from fastapi.responses import FileResponse as FAPI_FileResponse
+from fastapi import FastAPI as FAPI_FastAPI
 import gradio as gr
 import uvicorn
 import os
 
-app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-
-# ====================================================================================================
-
-gradio_app123 = gr.Interface(lambda aaaaa: "Hello, " + aaaaa + "!", "textbox", "textbox")
+fapi = FAPI_FastAPI()
+fapi.add_middleware(FAPI_CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 # ====================================================================================================
 
 # ENDPOINT: /root
-@app.get("/")
+fapi.mount("/root_static", FAPI_StaticFiles(directory="app/root/root_static"))
+@fapi.get("/")
 def endpoint_root():
-    return {"response": f"This is root!"}
+    return FAPI_FileResponse('app/root/index.html')
 
 # ENDPOINT: /gradio
-app = gr.mount_gradio_app(app, gradio_app123, path="/gradio")
+gradio_app123 = gr.Interface(lambda aaaaa: "Hello, " + aaaaa + "!", "textbox", "textbox")
+fapi = gr.mount_gradio_app(fapi, gradio_app123, path="/gradio")
 
 # ====================================================================================================
 
 uvicorn.run(
-    app, 
+    fapi, 
     host = "127.0.0.1" if not os.getenv("OS_ENV_DOCKER") else "0.0.0.0",
     port = 80,
 )
